566c017b282752182f90a07002c811cbaca292dc
Yuto Watanabe <yuto.w51942@gmail.com>
2021-06-13 21:50:38 +0900

    added sample module fibonacci

---
+++ fibonacci/cal.go
@@ -0,0 +1,28 @@
+package fibonacci
+
+// CalType is calculation type.
+// 1: 再帰
+// 2: for-loop
+type Calculate struct {
+	CalType int
+}
+
+func (cal *Calculate) Cal(input int64) (int64, error) {
+	var result int64
+
+	switch cal.CalType {
+	case 1:
+		result = calRecursion(input)
+	case 2:
+		break
+	}
+
+	return int64(result), nil
+}
+
+func calRecursion(a int64) int64 {
+	if a <= 1 {
+		return a
+	}
+	return calRecursion(a-1) + calRecursion(a-2)
+}
---
+++ fibonacci/go.mod
@@ -0,0 +1,3 @@
+module github.com/go-package-sample/fibonacci
+
+go 1.16
